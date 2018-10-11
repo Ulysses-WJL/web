736bb378aed1406610d734d03eb460cc224c05ea
RaiderTcT <jianliangwu1171@gmail.com>
2018-10-11 15:48:28 +0800

    user_setting

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # SECRET_KEY = 'role user test'
 #设置这一项是每次请求结束后都会自动提交数据库中的变动
 import os
-from user_setting import *
+# from user_setting import *
 class Config:
     # 防止CSRF攻击， Flask-WTF为所有表单生成安全令牌，根据秘钥生成
     SECRET_KEY = os.environ.get('SECRET_KKEY') or 'you never guess'
@@ -11,8 +11,8 @@ class Config:
     MAIL_PORT = os.environ.get('MAIL_PORT') or 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or qqmail_user
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") or qqmail_passwd
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = '2276777056@qq.com'
     FLASK_ADMIN = os.environ.get("FLASK_ADMIN") or 'jianliangwu1171@gmail.com'
